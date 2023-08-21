be472c98b4a376b813f7cc71b11d0e442cc8b8fc
yangchen <yangchen@hongkingsystem.cn>
2023-08-21 13:59:58 +0800

    update cmake

--- deps/build.sh
+++ deps/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #copy hiredis
-cp -r ./hiredis/*.h* ../include/hiredis
-cp -r ./hiredis/adapters/*.h ../include/hiredis/adapters/
+# cp -r ./hiredis/*.h* ../include/hiredis
+# cp -r ./hiredis/adapters/*.h ../include/hiredis/adapters/
 
-#copy redis++
-cp -r ./redispp/src/sw/redis++/*.h* ../include/redis/
-cp -r ./redispp/src/sw/redis++/recipes/*.h ../include/redis/recipes
+# #copy redis++
+# cp -r ./redispp/src/sw/redis++/*.h* ../include/redis/
+# cp -r ./redispp/src/sw/redis++/recipes/*.h ../include/redis/recipes
--- src/json/json_test.cpp
+++ src/json/json_test.cpp
@@ -1,5 +1,6 @@
 #include "json.hpp"
 #include <iostream>
+#include "defines.h"
 using namespace nlohmann;
 
 int main(){
@@ -12,5 +13,25 @@ int main(){
     doc["array"] = d1;
 
     std::cout<<doc.dump()<<std::endl;
+
+    std::string data = R"({"a":"b","array":[0,1,2,3,4,5,6,7,8,9]})";
+    json d;
+    try
+    {
+        auto d = json::parse(data);
+
+        std::cout<<d["a"]<<std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    if(likely(typeid(d) == typeid(json))){
+        std::cout<<"json type"<<std::endl;
+    }else if(likely(typeid(d) == typeid(std::string))){
+        std::cout<<"std::string type"<<std::endl;
+    }
+    
     return 0;
 }
